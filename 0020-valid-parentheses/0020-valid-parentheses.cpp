3445de7a78db0c7052b9c83db90eafbf0d77bd95
narend99 <122995550+narend99@users.noreply.github.com>
2024-01-18 14:04:09 +0530

    Time: 3 ms (45.21%), Space: 7 MB (15.59%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool isValid(string s) 
+    {
+         stack<char> st;
+    
+    for(int i=0;i<s.length();i++)
+    {
+        if(st.empty() || s[i] == '(' || s[i] == '{' || s[i] == '[')
+        {
+            st.push(s[i]);
+            continue;
+        }
+
+        if(s[i] == ')' && st.top() == '(')
+        {
+            st.pop();
+            continue;
+        }
+         
+        if(s[i] == '}' && st.top() == '{')
+        {
+            st.pop();
+            continue;
+        }
+         
+        if(s[i] == ']' && st.top() == '[')
+        {
+            st.pop();
+            continue;
+        }
+        
+        if(!st.empty())
+        {
+            st.push(s[i]);
+        }
+    }
+    
+    if(st.empty())
+    return true;
+    else 
+    return false;
+    }
+};
