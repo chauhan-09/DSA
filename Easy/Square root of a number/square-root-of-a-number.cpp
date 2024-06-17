8d5789c893ac537acbe1df603de07701cdc9f14d
narend99 <122995550+narend99@users.noreply.github.com>
2024-06-17 13:01:00 +0530

    Updated Solution - GfG to GitHub

--- Easy/Square root of a number/square-root-of-a-number.cpp
+++ Easy/Square root of a number/square-root-of-a-number.cpp
@@ -11,31 +11,8 @@ class Solution{
   public:
     long long int floorSqrt(long long int x) 
     {
-        long long int s = 0 , e = x - 1 , ans = 1;
-        long long mul = 1;
         
-        while(s<=e)
-        {
-            long long int mid = (s+e)/2;
-            mul = mid*mid;
-            
-            if(mul == x)
-            {
-                return mid;
-            }
-            
-            if(mul > x)
-            {
-                e = mid - 1;
-            }
-            else if(mul < x)
-            {
-               ans = max(ans,mid);
-               s = mid + 1;
-            }
-        }
-        
-        return ans;
+        return sqrt(x);
     }
 };
 
